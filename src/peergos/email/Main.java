a77acdf1ba1c714348a22599cfbd8c976948b7ef
kevin odwyer <kevodwyer@gmail.com>
2021-08-17 08:44:50 +0100

    change style of command line params

--- src/peergos/email/Main.java
+++ src/peergos/email/Main.java
@@ -22,12 +22,12 @@ public class Main {
                 try {
                     String username = args.getArg("username");
                     String password = args.getArg("password");
-                    String url = args.getArg("peergos.url");
-                    boolean isPublicServer = args.getBoolean("isPublicServer");
-                    String smtpHost = args.getArg("smtpHost");
-                    int smtpPort = args.getInt("smtpPort");
-                    String imapHost = args.getArg("imapHost");
-                    int imapPort = args.getInt("imapPort");
+                    String url = args.getArg("peergos-url");
+                    boolean isPublicServer = args.getBoolean("is-public-server");
+                    String smtpHost = args.getArg("smtp-host");
+                    int smtpPort = args.getInt("smtp-port");
+                    String imapHost = args.getArg("imap-host");
+                    int imapPort = args.getInt("imap-port");
 
                     emailBridge = EmailBridge.build(username, password, url, isPublicServer
                             , smtpHost, smtpPort, imapHost, imapPort);
@@ -40,16 +40,16 @@ public class Main {
             Arrays.asList(
                     new Command.Arg("username", "username that represents the email bridge in peergos", true),
                     new Command.Arg("password", "password for the associated email bridge username", true),
-                    new Command.Arg("peergos.url", "url of peergos instance where the email bridge user is registered", true),
-                    new Command.Arg("isPublicServer", "is url represented by peergos.url public", true),
-                    new Command.Arg("smtpHost", "smtp host name", true),
-                    new Command.Arg("smtpPort", "smtp port number", true),
-                    new Command.Arg("imapHost", "imap host", true),
-                    new Command.Arg("imapPort", "imap port number", true)
+                    new Command.Arg("peergos-url", "url of peergos instance where the email bridge user is registered", true),
+                    new Command.Arg("is-public-server", "is url represented by peergos.url public", true),
+                    new Command.Arg("smtp-host", "smtp host name", true),
+                    new Command.Arg("smtp-port", "smtp port number", true),
+                    new Command.Arg("imap-host", "imap host", true),
+                    new Command.Arg("imap-port", "imap port number", true)
             )
     );
     /*
-    java -jar EmailBridge.jar -username blah -password qwerty -peergos.url http://localhost:8000 -isPublicServer false -smtpHost smtpHost -smtpPort 1 -imapHost imapHost -imapPort 1
+    java -jar EmailBridge.jar -username blah -password qwerty -peergos-url http://localhost:8000 -is-public-server false -smtp-host smtpHost -smtp-port 1 -imap-host imapHost -imap-port 1
     example accounts.json contents:
     [{ "username": "test", "emailAddress": "", "smtpUsername": "", "smtpPassword": "", "imapUsername": "", "imapPassword": ""}]
      */
